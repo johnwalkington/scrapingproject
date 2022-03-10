0cc7d4679b974091010afa5233942200ce8466fa
johnwalkington <94403404+johnwalkington@users.noreply.github.com>
2022-03-10 15:46:54 -0600

    first scrape

--- scraper.py
+++ scraper.py
@@ -1,1 +1,10 @@
 # This scraper will take a geographic area and scrape all craigslist data #
+
+from requests import get
+from bs4 import BeautifulSoup
+
+response = get('https://austin.craigslist.org/')
+
+html_soup = BeautifulSoup(response.text, 'html.parser')
+
+print(html_soup)
