8b7a628193d5f37e96a137a53969e1365b8a5804
min jin kang <minjinkang@wireless-10-147-38-42.public.utexas.edu>
2022-03-11 18:44:01 -0600

    added year information

---
+++ code/year_column.py
@@ -0,0 +1,17 @@
+import pandas as pd
+df=pd.read_csv("/Users/minjinkang/Desktop/scrapingproject/code/Major_Cities_Moto_Data.csv")
+
+import re
+df=df[df['Price']!=0]
+df['year']=df['Product'].apply(lambda x: x.split()[-1] if x.split()[-1].isnumeric() and 
+	int(x.split()[-1])>= 2000 else x)
+
+df['year']=df['year'].apply(lambda x: x.split()[0] if x.split()[0].isnumeric() else x)
+
+df['year']=df['year'].apply(lambda x: x.split()[1] if x.split()[0].lower() == "used" else x)
+df['year']=df['year'].apply(lambda x: x if x.isnumeric() and len(x) ==4 else 'Unknown')
+
+final=df[df['year'] != 'Unknown']
+final=final.reset_index()[['Product','Price','year']]
+
+
